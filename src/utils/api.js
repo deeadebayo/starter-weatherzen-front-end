c39129083d5c3d3f260d431d2cf4ec3dcff2c35d
Dee Adebayo <deeadebayo@gmail.com>
2021-12-31 12:32:31 -0500

    mvp

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,13 +2,13 @@
  * Defines the base URL for the API.
  * The default values is overridden by the `API_BASE_URL` environment variable.
  */
-const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:5000";
+const API_BASE_URL = process.env.API_BASE_URL || 'http://localhost:5000'
 
 /**
  * Defines the default headers for these functions to work with `json-server`
  */
-const headers = new Headers();
-headers.append("Content-Type", "application/json");
+const headers = new Headers()
+headers.append('Content-Type', 'application/json')
 
 /**
  * Fetch `json` from the specified URL and handle error status codes and ignore `AbortError`s
@@ -24,23 +24,44 @@ headers.append("Content-Type", "application/json");
  *  If the response is not in the 200 - 399 range the promise is rejected.
  */
 async function fetchJson(url, options) {
-  try {
-    const response = await fetch(url, options);
+	try {
+		const response = await fetch(url, options)
 
-    if (response.status === 204) {
-      return null;
-    }
+		if (response.status === 204) {
+			return null
+		}
 
-    const payload = await response.json();
+		const payload = await response.json()
 
-    if (payload.error) {
-      return Promise.reject({ message: payload.error });
-    }
-    return payload.data;
-  } catch (error) {
-    if (error.name !== "AbortError") {
-      console.error(error.stack);
-      return Promise.reject({ message: error.message });
-    }
-  }
+		if (payload.error) {
+			return Promise.reject({ message: payload.error })
+		}
+
+		return payload.data
+	} catch (error) {
+		if (error.name !== 'AbortError') {
+			console.error(error.stack)
+			return Promise.reject({ message: error.message })
+		}
+	}
+}
+
+export async function createObservation(observation, signal) {
+	const url = `${API_BASE_URL}/observations`
+	const options = {
+		method: 'POST',
+		headers,
+		body: JSON.stringify({ data: observation }),
+		signal,
+	}
+	return await fetchJson(url, options)
+}
+
+export async function listObservations(signal) {
+	const url = `${API_BASE_URL}/observations`
+	const options = {
+		headers,
+		signal,
+	}
+	return await fetchJson(url, options)
 }
